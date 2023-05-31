9e0fc35d3424b3aa31fafb8a3673869885858c7b
HAMZA COMPUTER <HAMZA COMPUTER@TAHIR-PC>
2023-06-01 00:41:18 +0500

    Add project files.

---
+++ BookStoreApp.API/Migrations/20230531193632_SeededDefaultUserandRoles.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookStoreApp.API.Migrations
+{
+    public partial class SeededDefaultUserandRoles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "8343074e-8623-4e1a-b0c1-84fb8678c8f3", "7b6b6cd3-e7bd-40ba-8dcb-31de3895b277", "User", "USER" },
+                    { "c7ac6cfe-1f10-4baf-b604-cde350db9554", "8de66228-df4c-4a32-a47e-76fa8635ad41", "Administrator", "ADMINISTRATOR" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "FirstName", "LastName", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[,]
+                {
+                    { "30a24107-d279-4e37-96fd-01af5b38cb27", 0, "96a124a8-7c72-4e76-a740-b9ac1686a05a", "user@bookstore.com", false, "System", "User", false, null, "USER@BOOKSTORE.COM", "USER@BOOKSTORE.COM", "AQAAAAEAACcQAAAAEFD3V5W1VQHGLuhLHIJSBIIHRsZribeVt3pqJpXHpBSp7XIryMSwPjCSS8Wesqurgg==", null, false, "622035c4-0b8c-43ac-85b7-fc820171b0eb", false, "user@bookstore.com" },
+                    { "8e448afa-f008-446e-a52f-13c449803c2e", 0, "a11f86c5-3a2a-4f2a-afc0-c9063d9e9c96", "admin@bookstore.com", false, "System", "Admin", false, null, "ADMIN@BOOKSTORE.COM", "ADMIN@BOOKSTORE.COM", "AQAAAAEAACcQAAAAEFPAujkJjJ0y3NeQJSfYQWrzlQFFr6RNp5z+x1YMRBR8NPjfhV++V9h/W7Kii08nLA==", null, false, "15598669-8775-47ba-8102-8fed49ba8c2c", false, "admin@bookstore.com" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "8343074e-8623-4e1a-b0c1-84fb8678c8f3", "30a24107-d279-4e37-96fd-01af5b38cb27" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "c7ac6cfe-1f10-4baf-b604-cde350db9554", "8e448afa-f008-446e-a52f-13c449803c2e" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "8343074e-8623-4e1a-b0c1-84fb8678c8f3", "30a24107-d279-4e37-96fd-01af5b38cb27" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "c7ac6cfe-1f10-4baf-b604-cde350db9554", "8e448afa-f008-446e-a52f-13c449803c2e" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "8343074e-8623-4e1a-b0c1-84fb8678c8f3");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "c7ac6cfe-1f10-4baf-b604-cde350db9554");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "30a24107-d279-4e37-96fd-01af5b38cb27");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "8e448afa-f008-446e-a52f-13c449803c2e");
+        }
+    }
+}
